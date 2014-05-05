dfe79500fbbcc127f4f33d49691b96ecbbd6360a
Iain Andrew <iainbruceandrew@gmail.com>
2014-05-05 22:01:01 +0100

    added services section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 $(function() {
-	$(".enda img").hover(function() {
+	$(".enda a").hover(function() {
 		$(".read-more-enda").stop().animate({opacity:1}, {duration:500});
 	}, function() {
 		$(".read-more-enda").stop().animate({opacity:0}, {duration:500});
 	});
-	$(".ben img").hover(function() {
+	$(".ben a").hover(function() {
 		$(".read-more-ben").stop().animate({opacity:1}, {duration:500});
 	}, function() {
 		$(".read-more-ben").stop().animate({opacity:0}, {duration:500});
